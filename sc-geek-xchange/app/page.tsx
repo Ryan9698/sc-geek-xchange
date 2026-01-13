dca160f2a371df16cd3836e0b2df396a68184018
Ryan Caltabiano <rcaltabiano90@gmail.com>
2026-01-13 05:34:32 -0500

    small changes

--- sc-geek-xchange/app/page.tsx
+++ sc-geek-xchange/app/page.tsx
@@ -6,19 +6,8 @@ export default function Home() {
   return (
     <section className="">
       <main>
-        <header>
-          <h1 className="text-3xl font-bold text-center p-4">
-            Welcome to the Space Coast Geek XChange!
-          </h1>
-          <p className="p-4 text-center mx-auto">
-            We are a local business that offers pop culture collectibles for
-            both the budding enthusiast and serious collectors. We offer
-            memorabilia from somme of the most influential and iconic
-            franchises. Whether you are looking for a Darth Vader lightsaber or
-            a niche model; we have you covered.
-          </p>
-        </header>
-        <Header />
+        <header className="text-end"></header>
+        {/* <Header /> */}
         <Hero />
       </main>
     </section>
--- sc-geek-xchange/components/sections/Hero.tsx
+++ sc-geek-xchange/components/sections/Hero.tsx
@@ -2,9 +2,13 @@
 export default function Hero() {
   return (
     <section className="py-24 text-center">
-      <h1 className="text-4xl font-bold text-green-300">
+      <h1 className="text-6xl ">Space</h1>
+      <h2 className="text-6xl mr-8">Coast</h2>
+      <h3 className="text-6xl ">Geek </h3>
+      <h4 className="text-4xl">XChange</h4>
+      <p className="text-4xl font-bold text-green-300">
         Collectibles for True Geeks
-      </h1>
+      </p>
       <p className="mt-4 text-zinc-400">
         Comics, figures, and fandom treasures
       </p>
